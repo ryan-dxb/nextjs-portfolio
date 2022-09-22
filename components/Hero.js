dd2b953b9908d14b44f20e145e96659818d2ec32
ryan-dxb <ryan.dxb92@gmail.com>
2022-09-22 10:12:30 +0400

    Added Hero Section with Nav menu

---
+++ components/Hero.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { Cursor, useTypewriter } from "react-simple-typewriter";
+import BackgroundCircles from "./BackgroundCircles";
+import Pic from "../public/pro-pic.jpg";
+import Image from "next/image";
+import Link from "next/link";
+
+function Hero() {
+  const [text, count] = useTypewriter({
+    words: [
+      "Hi, The Name's Ryan Pereira",
+      "Python & Javascript Developer",
+      "Entrepreneur, Developer, Designer",
+    ],
+    loop: true,
+    delaySpeed: 2000,
+  });
+
+  return (
+    <div className="flex flex-col h-screen space-y-8 items-center justify-center text-center overflow-hidden">
+      <BackgroundCircles />
+      <img
+        className="relative rounded-full h-32 w-32 mx-auto object-cover"
+        src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2080&q=80"
+        alt=""
+      />
+      <div className="z-20">
+        <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">
+          Software Developer
+        </h2>
+        <h1 className="text-5xl lg:text-6xl font-semibold px-10">
+          <span className="mr-3">{text}</span>
+          <Cursor cursorColor="#f7ABBA" />
+        </h1>
+
+        <div className="pt-5">
+          <Link href="#about">
+            <button className="heroButton">About</button>
+          </Link>
+          <Link href="#experience">
+            <button className="heroButton">Experience</button>
+          </Link>
+          <Link href="#skills">
+            <button className="heroButton">Skills</button>
+          </Link>
+          <Link href="#projects">
+            <button className="heroButton">Projects</button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,27 @@
 import Head from "next/head";
 import Header from "../components/Header";
+import Hero from "../components/Hero";
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
       <Head>
         <title>Ryan Portfolio</title>
       </Head>
 
+      {/* Header */}
       <Header />
+      {/* End Header */}
+
+      {/* Hero */}
+      <section id="hero" className="snap-center">
+        <Hero />
+      </section>
+      {/* End Hero */}
+
+      {/* About */}
+
+      {/* End About */}
     </div>
   );
 }
